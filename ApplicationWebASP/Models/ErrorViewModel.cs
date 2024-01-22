7a5325468198668728cb75631df7a6f20f890bf5
Egor Osadchuk <gorlum1.osadchuk@yandex.com>
2024-01-22 20:44:31 +0300

    Тестовая отправка

---
+++ ApplicationWebASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApplicationWebASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

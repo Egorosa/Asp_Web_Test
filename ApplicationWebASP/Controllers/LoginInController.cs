d933cbf43f734eb6cdcf1a0c9f0a5e4e52376f5c
Егор Осадчук <gorlum1@outlook.com>
2024-01-28 23:57:58 +0300

    Добавление контроллеров  и моделей

---
+++ ApplicationWebASP/Controllers/LoginInController.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationWebASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationWebASP.Controllers
+{
+    public class LoginInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Fitback fitback)
+        {
+            if (ModelState.IsValid) {
+                return Redirect("/");
+            }
+            return View("Index");
+            
+        }
+    }
+}
---
+++ ApplicationWebASP/Models/FitBack.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationWebASP.Models
+{
+    public class Fitback
+    {
+        [Display (Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}

34e2cce247c6e4bd15308654a804cf0a8e866650
Egor <gorlum1@outlook.com>
2024-01-29 16:18:13 +0300

    Исправление обработки формы

--- ApplicationWebASP/Models/FitBack.cs
+++ ApplicationWebASP/Models/FitBack.cs
@@ -10,7 +10,9 @@ namespace ApplicationWebASP.Models
         public string SurName { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
+        [Display (Name = "Оставьте сообщение")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        [StringLength (30, ErrorMessage = "Текст не менее 30 символов")]
         public string Message { get; set; }
-
     }
 }
